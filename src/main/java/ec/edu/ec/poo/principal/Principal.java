71f574050336ac0c7490b0ceb1cc044945d756fa
Pablo Feijo <pfeijo34@gmail.com>
2025-03-31 12:06:33 -0500

    FIX(UPS-003): Actualizacion Notificacion de Cambios

--- src/main/java/ec/edu/ec/poo/principal/Principal.java
+++ src/main/java/ec/edu/ec/poo/principal/Principal.java
@@ -16,6 +16,7 @@ public class Principal {
 
 
         // Ingreso de personas
+        System.out.println("NOTIFICACION DE PABLO");
         System.out.println("Ingrese la cantidad de personas:");
         int nPersonas = scanner.nextInt();
         Persona[] personas = new Persona[nPersonas];
